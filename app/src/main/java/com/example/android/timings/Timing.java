128ff604d8981dc648f17a1b7715582b7d268882
arturiosarh <116462320+arturiosarh@users.noreply.github.com>
2023-04-04 01:50:21 +0300

    new

--- app/src/main/java/com/example/android/timings/Timing.java
+++ app/src/main/java/com/example/android/timings/Timing.java
@@ -71,8 +71,8 @@ public class Timing extends Activity {
     private String nowTimeBeginFull;
 
     private Timer buttonRunner;
-    private AlarmManager alarmManager1;
-    private AlarmManager alarmManager2;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
 
     private final Context context;
     private LinearLayout linearLayout1;
@@ -498,9 +498,9 @@ public class Timing extends Activity {
 
                     Intent intent01 = new Intent(context, MyReceiver.class);
 
-                    alarmManager1 = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-                    PendingIntent pendingIntent1 = PendingIntent.getBroadcast(context, NOTIFY_ID, intent01, PendingIntent.FLAG_IMMUTABLE);
-                    alarmManager1.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + duration01.toMillis() - 1001, pendingIntent1);
+                    alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+                    pendingIntent = PendingIntent.getBroadcast(context, NOTIFY_ID, intent01, PendingIntent.FLAG_IMMUTABLE);
+                    alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + duration01.toMillis() - 1001, pendingIntent);
 
                     textView1_1_1_01.setText(NumberFormat.getNumberInstance().format(laps));
                     myTimer = new Timer();  //добавление таймера
@@ -563,10 +563,9 @@ public class Timing extends Activity {
                                         .plusMinutes(beginMinutes);
                                 Duration duration2 = Duration.between(localDateTime2,localDateTime1);
                                 //Instant instant1 = localDateTime1.toInstant(ZoneOffset.UTC);
-                                alarmManager2 = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
                                 Intent intent02 = new Intent(context, MyReceiver.class);
-                                PendingIntent pendingIntent2 = PendingIntent.getBroadcast(context, NOTIFY_ID, intent02, PendingIntent.FLAG_IMMUTABLE);
-                                alarmManager2.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + duration2.toMillis(), pendingIntent2);
+                                pendingIntent = PendingIntent.getBroadcast(context, NOTIFY_ID, intent02, PendingIntent.FLAG_IMMUTABLE);
+                                alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + duration2.toMillis(), pendingIntent);
                                 textView1_1_1_01.setText(NumberFormat.getNumberInstance()
                                         .format(laps));
                                 nm = (NotificationManager) context.
@@ -598,6 +597,7 @@ public class Timing extends Activity {
                         Toast.LENGTH_SHORT).show();
                 if (myTimer != null) {
                     myTimer.cancel();
+                    alarmManager.cancel(pendingIntent);
                 }
                 timer = LocalTime.of(0, 0, 0);
                 time = dtf.format(timer);
